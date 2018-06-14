02956119296cea09d880a2f1c8b625ebcf041e26
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2018-06-13 21:50:17 -0500

    site is refershed

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.static('public'));
 
 
+//app.listen(3000); for local use only
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
